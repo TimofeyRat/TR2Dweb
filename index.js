cd494acd4b44274ea36041a8fbebf430d548d608
TimRat <timofejhismatulin@gmail.com>
2022-11-13 16:45:30 +0500

    Added texture rect support

--- index.js
+++ index.js
@@ -1,48 +1,74 @@
 var display = document.getElementById("display");
 var context = display.getContext('2d');
-display.style.left = "0px";
-display.style.right = "0px";
-display.style.position = "relative";
+var lastTime = Date.now();
+
 window.onscroll = () => { window.scroll(0, 0); }
 document.body.style.overflow = "hidden";
 
-var lastTime = Date.now();
-
-var image = new Image();
-image.onload = function() { startGame(); };
-image.src = 'res/test.png';
-var imageX = 0;
-var imageY = 0;
-var moveRight = true;
-
-function startGame()
+class Rectangle
 {
-	main();
+	x = 0; y = 0; w = 0; h = 0;
+	constructor(X = 0, Y = 0, W = 0, H = 0) { this.x = X; this.y = Y; this.w = W; this.h = H; }
 }
 
-document.addEventListener("touchstart", trTouch);
-document.addEventListener("touchmove", trTouch);
+class Object
+{
+	image = new Image();
+	posX = 0;
+	posY = 0;
+	texRect = new Rectangle();
+	constructor() { this.image.onload = function() { main(); }; }
+	setPos(X, Y) { this.posX = X; this.posY = Y; }
+	setImage(path) { this.image.src = path; }
+	containsPoint(pointX, pointY)
+	{
+		var contains = true;
+		if (this.posX > pointX) { contains = false; } if (this.posY > pointY) { contains = false; }
+		if (this.posX + this.image.width < pointX) { contains = false; }
+		if (this.posY + this.image.width < pointY) { contains = false; }
+		return contains;
+	}
+	setTextureRect(rect = new Rectangle())
+	{
+		this.texRect = rect;
+	}
+	draw()
+	{
+		if (this.texRect.w == 0 || this.texRect.h == 0) { context.drawImage(this.image, this.posX, this.posY); }
+		else
+		{
+			context.drawImage(this.image,
+				this.texRect.x, this.texRect.y, this.texRect.w, this.texRect.h,
+				this.posX, this.posY, this.texRect.w, this.texRect.h);
+		}
+	}
+}
+
+let obj = new Object();
+obj.setPos(100, 100);
+obj.setImage('res/test.png');
+obj.setTextureRect(new Rectangle(0, 0, 192, 192 / 2));
 
 var click = null;
 
-function trTouch(event)
-{
-	click = event.touches[0];
-}
+display.width = window.innerWidth; display.height = window.innerHeight;
+document.addEventListener("touchstart", function(event) { click = event.touches; });
+document.addEventListener("touchmove", function(event) { click = event.touches; });
+document.addEventListener("resize", function() { display.width = window.innerWidth; display.height = window.innerHeight; });
 
 function update(deltaTime)
 {
 	if (click == null) return;
-	imageX = click.pageX - image.width / 2;
-	imageY = click.pageY - image.height / 2;
-	display.width = window.innerWidth;
-	display.height = window.innerHeight;
+	if (obj.containsPoint(click[0].pageX, click[0].pageY) === true)
+	{
+		obj.setPos(click[0].pageX - obj.image.width / 2, click[0].pageY - obj.image.height / 2);
+	}
 }
 
 function render()
 {
 	context.clearRect(0, 0, display.width, display.height);
-	context.drawImage(image, imageX, imageY);
+	obj.draw();
 }
 
 function main()
